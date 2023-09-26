319080dd1b9412faad3e29c615e079a97137b248
Esteban Castro <136758220+EcastroGit@users.noreply.github.com>
2023-09-26 15:59:35 -0500

    apilink v1.1.0

---
+++ run.py
@@ -0,0 +1,6 @@
+from app import app
+
+if __name__ == '__main__':
+    app.config.from_object('config.app_config.development')
+    app.register_error_handler(404, app.page_not_found)
+    app.run(port=app.config['PORT'])
